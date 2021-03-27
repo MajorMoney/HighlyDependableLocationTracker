1f3b8928075463280ed6cf6900e51783129f192b
MajorMoney <penuno005@gmail.com>
2021-03-27 17:07:51 +0000

    Initial commit

---
+++ STDFProject/src/main/java/pt/ist/stdf/STDFProject/App.java
@@ -0,0 +1,13 @@
+package pt.ist.stdf.STDFProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
